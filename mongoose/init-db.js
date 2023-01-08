6e355b65b74a255053a31908548a81a1275ec209
Laura Perote <laury0luz@example.com>
2023-01-08 17:38:29 +0100

    Added security question

--- mongoose/init-db.js
+++ mongoose/init-db.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const fs = require('fs');
-const Addvert = require("./models/item.js")
+const readline = require('readline');
+const Addvert = require("../models/item.js")
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
+const rl=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Are you sure yo want to make changes to the database? y/n: ", async(answer) => {  
+    if(answer ==="y"){
+        mongoose.set('strictQuery', false);
+        mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
+    
+        // To read the JSON using file sistem as we have to get the json from a file
+        const data = JSON.parse(fs.readFileSync('./mongoose/adverts.json'));
+    
+        //We create an add model and export it
+        await Addvert.create(data)
+    }  
+    rl.close();
+});
 
-// To read the JSON using file sistem as we have to get the json from a file
-const data = JSON.parse(fs.readFileSync('./mongoose/adverts.json'));
-
-//We create an add model and export it
-Addvert.create(data)
+rl.on('close', () => process.exit(0));
